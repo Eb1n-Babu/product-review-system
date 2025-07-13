5f3c17686b6b7512ac560ebec02de8ef6d2f4198
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-07-13 15:11:52 +0530

    Secure SECRET_KEY using environment variables

--- product_review/product_review/settings.py
+++ product_review/product_review/settings.py
@@ -10,17 +10,27 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+#from pathlib import Path
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv('django-insecure-%0*hg#uy24u$q#(7n3qfc%s14x877kua+q(=_4bj!uxjw-l276')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%0*hg#uy24u$q#(7n3qfc%s14x877kua+q(=_4bj!uxjw-l276'
+#SECRET_KEY = 'django-insecure-%0*hg#uy24u$q#(7n3qfc%s14x877kua+q(=_4bj!uxjw-l276'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
